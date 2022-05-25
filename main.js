941d39464e5a3dd8c8517c6389832448c41f784b
DANIELGEDAMO <danielgadamo@gmail.com>
2022-05-25 09:15:22 +0300

    צ

---
+++ main.js
@@ -0,0 +1,83 @@
+// function printRandomNumber(){
+//     var  rndNum = Math.floor(Math.random()*10);
+//     console.log(rndNum);
+// // }
+// function returnRandomNumber(){
+//     var rndNum = (Math.random()*11);
+//     return rndNum
+// }
+// returnRandomNumber ()
+// function returnFrom0to10(num) {
+//     // var num = +prompt
+//     var randomNum = Math.floor(Math.random()*num+1);
+   
+//     return randomNum
+// }
+// returnFrom0to10()
+// function returnSeccesOrFail (){
+//    var userNum=  +prompt ('enter num')
+//     var randomNum = Math.floor(Math.random()*11)
+//     if (randomNum ==userNum){
+//         document.write ('succes')
+//     }
+//     else{
+//         document.write('fail')
+//     }
+// }returnSeccesOrFail()
+// function popRnadomArray (){
+//     var randomAraay =[];
+//     for(var i= 0; i<10; i++){
+//         randomAraay.push(Math.floor(Math.random()*100))
+//     }
+//   return randomAraay
+// } popRnadomArray ()
+// function getTheBigstNum (){
+//     var userNum = +prompt('enter number')
+//     var myNum =Math.floor(Math.random()*11)
+//     return Math.max(userNum,myNum)
+                            //   kלחזור לזה
+
+// }getTheBigstNum ()
+// function prinLowestNumber (){
+//     var array = [];
+//     for(var i =0;i<=5;i++){
+//       var userNum =  +prompt('enter number')
+      
+//     }
+//     return Math.min(i)
+// }prinLowestNumber()
+// function rndNumTo100(){
+//     var rndNum= Math.floor(Math.random()*101);
+//      var userNum= +prompt('enter num');
+//      if (userNum %2==0) {
+//        return Math.min (rndNum,userNum); 
+//      }
+//      else{
+//          return Math.max (rndNum,userNum);
+//      }
+    
+    
+// }rndNumTo100();
+// function getRndArray (){
+//     var array =[];
+//     var rndindex = Math.floor(Math.random()*365);
+//     for( var i=0; i<rndindex; i++){
+//         array.push (rndNUM)
+//     var rndNUM = Math.floor(Math.random()*874);
+//     }
+//     return array;
+// // }getRndArray()
+//                                            -------------------------------לחזור לזה    
+function getArray (array){
+ var rndindex = Math.floor(Math.random()*array)
+ document.write(rndindex[i])
+}getArray(['68 78 89 90 56'])
+
+
+
+// function getNameAndReplace(){
+// var name = prompt("enter name")
+// var rndindex= Math.floor(Math.random()*name.length)
+
+// }getNameAndReplace()
+     
